021cdca2e656d48fd00076c35e9546bdb87ef13b
Dnyaneshwar Ratan Bhalerao <dnyaneshrbhalerao@gmail.com>
2022-10-07 12:41:04 +0530

    Add sanitization function

---
+++ main.js
@@ -0,0 +1,17 @@
+const updateObject = (objRef, objKey, callBack) => {
+  if(typeof callback === 'function'){
+    objRef[objKey] = callBack(objRef[objKey])
+  }
+  return objRef;
+}
+export const sanitizeObject= (obj, callBack) => {
+  if(typeof obj === 'object' && obj !== null) {
+      Object.keys(obj).forEach(objKey => {
+        if(typeof obj[objKey] === 'string') {
+              return updateObject(obj, objKey, callBack)
+        }
+        return sanitizeObject(obj[objKey], callBack)
+      })
+  }
+  return obj;
+}
